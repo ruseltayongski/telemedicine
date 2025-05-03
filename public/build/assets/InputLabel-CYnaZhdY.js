e1654ddd730aa9fdcbcd750ca14d8a739d1f9cec
Rusel T. Tayong <ruseltayong@gmail.com>
2025-05-03 19:35:16 +0800

    Push updates

--- public/build/assets/InputLabel-CYnaZhdY.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-TaxcoSw-.js";function l({value:e,className:r="",children:m,...s}){return t.jsx("label",{...s,className:"form-label fw-medium "+r,children:e||m})}export{l as I};
---
+++ public/build/assets/index-DjsuGO5n.js
@@ -0,0 +1,1 @@
+import{b as r}from"./app-D0-MmawO.js";var a=r();export{a as r};
